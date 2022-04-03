32ad028c447cde5b4ea7c1fa03a4b4f081c55894
DogGang <rlaehgusqp@naver.com>
2022-04-03 15:25:08 +0900

    footer  추가

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ function Footer(){
             <div className={styles.wrap}>
                 <p>MBTI로 대학전공 적성찾기</p>
                 <p>Email : rlaehgusqp@naver.com</p>
-                <p>COPYRIGHT© 2022 DogGang. ALL RIGHTS RESERVED.</p>
+                <p>COPYRIGHT© 2022 <a className={styles.link} href='https://github.com/doggang'>DogGang.</a> ALL RIGHTS RESERVED.</p>
             </div>
         </footer>
     );
