0244e3abdd621d422c0526f37a3cf3eb05011a94
DogGang <rlaehgusqp@naver.com>
2022-04-03 15:28:28 +0900

    header onclick 추가

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
 import headerImg from '../../img/brain.png';
 import styles from './header.module.css';
 
-function Header(){
+function Header(props){
   return(
     <header>
-      <a className={styles.header} href='#'>
+      <a className={styles.header} onClick={function(e){
+        e.preventDefault();
+        props.onChangeMode();
+      }}>
         <img className={styles.img} src={headerImg} alt="" />
         <p className={styles.title}>MBTI로 전공찾기</p>
       </a>
