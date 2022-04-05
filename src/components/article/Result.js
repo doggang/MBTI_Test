a7ae73dc9c05a3d5dc7a0e53a8d48bb36cbe99d4
DogGang <rlaehgusqp@naver.com>
2022-04-05 21:11:45 +0900

    결과창 추가

---
+++ src/components/article/Result.js
@@ -0,0 +1,13 @@
+import styles from './result.module.css';
+
+function Result(){
+    return(
+        <return>
+            <div className={styles.wrap}>
+                asdf
+            </div>
+        </return>
+    );
+}
+
+export default Result;
